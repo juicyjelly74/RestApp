7e4303817c908ea4570b3eef831eedfa2585383f
juicyjelly74 <juicy.jelly74@gmail.com>
2018-01-02 19:14:39 +0300

    [+] Add frontend

---
+++ RestApp/Controllers/HomeController.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RestApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Contact page.";
+
+            return View();
+        }
+
+        public ActionResult Menu()
+        {
+            ViewBag.Message = "Menu.";
+
+            return View();
+        }
+
+        public ActionResult Restaurants()
+        {
+            ViewBag.Message = "Restaurants.";
+
+            return View();
+        }
+        public ActionResult Cart()
+        {
+            ViewBag.Message = "Cart page.";
+
+            return View();
+        }
+        public ActionResult Reserve()
+        {
+            ViewBag.Message = "Reserve page.";
+
+            return View();
+        }
+
+        public ActionResult Thank()
+        {
+            ViewBag.Message = "Thank you page.";
+
+            return View();
+        }
+
+        public ActionResult ReservationCompleted()
+        {
+            ViewBag.Message = "Reservation page.";
+
+            return View();
+        }
+
+    }
+}
---
+++ SQLQuery1.sql
@@ -0,0 +1,17 @@
+﻿CREATE TABLE [dbo].[Restaurant_Users]
+(
+	[Id] INT NOT NULL IDENTITY ,
+	[Username] NVARCHAR(50) NOT NULL,
+	[Password] NVARCHAR(MAX) NOT NULL,
+	[RegDate] DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+	[Email] NVARCHAR(50) NOT NULL,
+	PRIMARY KEY ([Id])
+)
+GO
+CREATE INDEX [IX_Restaurant_Users_Username] ON [dbo].[Restaurant_Users] ([Username])
+GO
+INSERT INTO [dbo].[Restaurant_Users]
+	([Username], [Password], [Email])
+VALUES
+	('test', 'test', 'test@test.test')
+GO
