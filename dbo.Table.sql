8658c22ff9ba1a9fe1af5c5100b9f9cbd2fb6a75
juicyjelly74 <juicy.jelly74@gmail.com>
2018-01-04 23:08:52 +0300

    Revert "[+] Added database model and Dish controller"

--- dbo.Table.sql
+++
@@ -1,9 +0,0 @@
-﻿CREATE TABLE [dbo].[Menu]
-(
-	[Id] INT NOT NULL PRIMARY KEY, 
-    [Name] NVARCHAR(100) NOT NULL, 
-    [Description] NVARCHAR(MAX) NULL, 
-    [Price] DECIMAL NULL, 
-    [Image] IMAGE NULL, 
-    [Category] NVARCHAR(100) NOT NULL
-)
