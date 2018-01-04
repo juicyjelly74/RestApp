0d46490ed9dfc5f586851c757f6106e0b7b26c3e
nastyagurskaya <just.gurskaya@gmail.com>
2018-01-04 22:56:30 +0300

    Added database model and Dish controller

--- RestApp/Controllers/HomeController.cs
+++ RestApp/Controllers/HomeController.cs
@@ -3,15 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-using RestApp.Models;
 
 namespace RestApp.Controllers
 {
     public class HomeController : Controller
     {
-        private RestaurantUserDatabaseEntities db = new RestaurantUserDatabaseEntities();
-        private Restaurant_Users currentUser = null;
-
         public ActionResult Index()
         {
             return View();
@@ -20,7 +16,6 @@ namespace RestApp.Controllers
         public ActionResult About()
         {
             ViewBag.Message = "Description page.";
-
             return View();
         }
 
@@ -71,61 +66,5 @@ namespace RestApp.Controllers
             return View();
         }
 
-        public ActionResult Login()
-        {
-            ViewBag.Message = "Login page.";
-
-            return View();
-        }
-
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Login([Bind(Include = "Username,Password")] Restaurant_Users user)
-        {
-            if (ModelState.IsValid)
-            {
-                var obj = db.Restaurant_Users.Where(a => a.Username.Equals(user.Username)
-                            && a.Password.Equals(user.Password)).FirstOrDefault();
-                if (obj != null)
-                {
-                    currentUser = user;
-                    Session["User"] = user.Username.ToString();
-                    return RedirectToAction("Index");
-                }
-            }
-
-            ViewBag.User = user;
-            return View(user);
-        }
-
-        public ActionResult Logout()
-        {
-            currentUser = null;
-            Session["User"] = null;
-                   
-            ViewBag.User = null;
-            return RedirectToAction("Index");
-        }
-
-        public ActionResult Register()
-        {
-            ViewBag.Message = "Register page.";
-
-            return View();
-        }
-
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Register([Bind(Include = "Username,Password,Email")] Restaurant_Users user)
-        {
-            if (ModelState.IsValid)
-            {
-                user.RegDate = DateTime.Now;
-                db.Restaurant_Users.Add(user);
-                db.SaveChanges();
-                return RedirectToAction("Login");
-            }
-            return View(user);
-        }
     }
 }
